c3fc2b5cbb9a15e1a7d210e6ea5f811ec46308c2
Roland <rolandlifejr@gmail.com>
2024-03-04 14:04:18 -0700

    added add command

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import "fmt"
+import "github.com/RJ-github/go-todo/cmd"
 
 func main() {
-    fmt.Println("Hello world");
+	cmd.Execute()
 }
